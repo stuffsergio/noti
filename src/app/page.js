70793440b04ed49f91f032ed184b35d24c1eb192
stuffsergio <sergioperezmontalvo@gmail.com>
2026-01-19 14:52:12 +0100

    faltaba algo

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import DeviceGate from "@/components/DeviceGate";
 import EnableNotifications from "@/components/EnableNotifications";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
       <main>
         <EnableNotifications />
       </main>
+      <DeviceGate />
     </div>
   );
 }
